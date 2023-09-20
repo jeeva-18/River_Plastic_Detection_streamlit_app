4c8dc535924a9bf46c57a7a33f3c19935939dfff
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-09-20 13:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -183,22 +183,36 @@ if uploaded_file is not None:
 	    color = [1.0, 0.5, 0, 0.2]
 	    st.sidebar.header("Geolocation:",divider='rainbow')
 	    st.sidebar.map(df,color=color)
-	    st.pydeck_chart(pdk.Deck(map_style=None,initial_view_state=pdk.ViewState(
+	    chart_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+				   columns=['lat', 'lon'])
+	    st.pydeck_chart(pdk.Deck(
+				    map_style=None,
+				    initial_view_state=pdk.ViewState(
 				        latitude=37.76,
 				        longitude=-122.4,
 				        zoom=11,
-				        pitch=50,),
-					    layers=[
-				      pdk.Layer(
-					   'HexagonLayer',
-					           data=df,
-					           get_position='[lon, lat]',
-					           radius=200,
-					           elevation_scale=4,
-					           elevation_range=[0, 1000],
-					           pickable=True,
-					           extruded=True,
-					        )]))
+				        pitch=50,
+				    ),
+				    layers=[
+				        pdk.Layer(
+				           'HexagonLayer',
+				           data=chart_data,
+				           get_position='[lon, lat]',
+				           radius=200,
+				           elevation_scale=4,
+				           elevation_range=[0, 1000],
+				           pickable=True,
+				           extruded=True,
+				        ),
+				        pdk.Layer(
+				            'ScatterplotLayer',
+				            data=chart_data,
+				            get_position='[lon, lat]',
+				            get_color='[200, 30, 0, 160]',
+				            get_radius=200,
+				        ),
+				    ],
+				))
   else:
      st.write("doesn't have exif data")
 if uploaded_file is not None:
